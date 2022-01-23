b1d8fde54c6c78622d9f8eae9c75c20a3d696ee8
Arunanshu Biswas <mydellpc07@gmail.com>
2022-01-23 14:31:53 +0530

    feat(init): make a major shift to Lua from VimL
    Finally a single file configuration. Flat is better than nested!

---
+++ .config/nvim/init.lua
@@ -0,0 +1,405 @@
+-- vim: fdm=marker:
+--[[
+File: init.lua
+Author: Arunanshu Biswas (@arunanshub)
+Description: A `init.vim` for the IDE beauty. Make sure you atleast have node
+installed. You may need to install other binaries depending on the coc
+extension.
+]]
+
+-- Install packer {{{1
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.cmd [[
+augroup Packer
+autocmd!
+autocmd BufWritePost init.lua PackerCompile
+augroup end
+]]
+-- 1}}}
+
+-- Plugins {{{1
+local use = require('packer').use
+require('packer').startup(function()
+  -- Package manager
+  use 'wbthomason/packer.nvim'
+
+  -- use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
+
+  -- Automatic tags management
+  use 'ludovicchabant/vim-gutentags'
+
+  -- Fancier statusline
+  use 'nvim-lualine/lualine.nvim'
+
+  -- Add indentation guides even on blank lines
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- Highlight, edit, and navigate code using a fast incremental parsing library
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- bracket autocompletion
+  use 'vim-scripts/auto-pairs-gentle'
+
+  -- "gc" to comment visual regions/lines
+  use 'numToStr/Comment.nvim'
+
+  -- FZF
+  use {
+    'junegunn/fzf.vim',
+    requires = {'junegunn/fzf', run = 'call fzf#install()'},
+  }
+
+  -- Git signs
+  use {'mhinz/vim-signify', run = {'Files', 'BLines', 'GFiles', 'Rg'}}
+
+  -- Git commands in nvim
+  use 'tpope/vim-fugitive'
+
+  -- surround text with stuff
+  use 'tpope/vim-surround'
+
+  -- useful mappings
+  use 'tpope/vim-unimpaired'
+
+  -- editorconfig support for neovim
+  use 'editorconfig/editorconfig-vim'
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+
+  -- LSP customizations
+  use 'williamboman/nvim-lsp-installer'
+  use 'tami5/lspsaga.nvim'
+  use 'onsails/lspkind-nvim'
+
+  -- Autocompletion plugin
+  use {'hrsh7th/nvim-cmp', requires = {'hrsh7th/cmp-nvim-lsp'}}
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+
+  -- snippets
+  use {
+    'hrsh7th/cmp-vsnip', requires = {
+      'hrsh7th/vim-vsnip',
+      'rafamadriz/friendly-snippets',
+    }
+  }
+
+  -- colorscheme
+  use 'navarasu/onedark.nvim'
+end)
+-- 1}}}
+
+-- Neovim settings {{{1
+--Make line numbers default
+vim.wo.number = true
+
+-- Show chars at the end of line
+vim.opt.list = true
+
+--Enable break indent
+vim.o.breakindent = true
+
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+--Set colorscheme
+vim.o.termguicolors = true
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menu,menuone'
+
+--Remap for dealing with word wrap
+vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+
+-- Highlight on yank
+vim.cmd [[
+augroup YankHighlight
+autocmd!
+autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+augroup end
+]]
+
+vim.cmd [[
+cnoreabbrev W w
+]]
+-- 1}}}
+
+-- Plugin settings {{{1
+
+-- lspconfig and LSP installer {{{2
+require('lspconfig')
+local lsp_installer = require("nvim-lsp-installer")
+
+-- Install required servers {{{3
+local servers = {
+  "bashls",
+  "pyright",
+  "rust_analyzer",
+  "sumneko_lua",
+  "html",
+}
+
+for _, name in pairs(servers) do
+  local server_is_found, server = lsp_installer.get_server(name)
+  if server_is_found and not server:is_installed() then
+    print("Installing " .. name)
+    server:install()
+  end
+end
+-- 3}}}
+
+local on_attach = function(_, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local opts = {noremap=true, silent=true}
+
+  buf_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua require("lspsaga.provider").lsp_finder()<CR>', opts)
+  buf_set_keymap('n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']g', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<C-f>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
+  buf_set_keymap('n', '<C-b>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
+  buf_set_keymap('n', '<leader>rn', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
+  buf_set_keymap('n', '<leader>ac', "<cmd>lua require('lspsaga.codeaction').code_action()<CR>", opts)
+  buf_set_keymap('v', '<leader>a', "<cmd>lua require('lspsaga.codeaction').range_code_action()<CR>", opts)
+end
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Additional server options {{{3
+local enhanced_server_opts = {
+  pylsp = function(opts)
+    opts.settings = {
+      pylsp = {
+        plugins = {
+          jedi_completion = {include_params = true},
+        }
+      }
+    }
+  end,
+
+  sumneko_lua = function(opts)
+    opts.settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' }
+        }
+      }
+    }
+  end
+}
+-- 3}}}
+
+lsp_installer.on_server_ready(function(server)
+  local opts = {
+    on_attach = on_attach,
+    flags = {debounce_text_changes = 150},
+    capabilities = capabilities,
+  }
+
+  if enhanced_server_opts[server.name] then
+    enhanced_server_opts[server.name](opts)
+  end
+
+  server:setup(opts)
+end)
+-- 2}}}
+
+-- nvim-cmp: Autocompletion plugin {{{2
+local lspkind = require('lspkind')
+local cmp = require("cmp")
+
+local has_words_before = function()
+  ---@diagnostic disable-next-line: deprecated
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+-- Fancy Autocompletion popup icons {{{3
+local cmp_kinds = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "ﰠ",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "塞",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "פּ",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+-- 3}}}
+
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      with_text = true,
+      preset = 'codicons',
+      symbol_map = cmp_kinds,
+      menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[Latex]",
+      }),
+    }),
+  },
+
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+  },
+
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'buffer' },
+  },
+}
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+-- 2}}}
+
+-- onedark
+require("onedark").setup({
+  style = "darker",
+})
+require('onedark').load()
+
+-- Lualine
+require('lualine').setup()
+
+-- Comment
+require('Comment').setup()
+
+-- indent-blankline
+require("indent_blankline").setup {
+  -- for example, context is off by default, use this to turn it on
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
+
+  filetype_exclude = { 'help', 'packer' },
+  buftype_exclude = { 'terminal', 'nofile' },
+  show_trailing_blankline_indent = false,
+}
+
+-- treesitter
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {"python", "rust", "c", "cpp", "bash", "go", "html"},
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+}
+
+-- FZF
+vim.cmd [[
+nnoremap <C-P> :Files<CR>
+nnoremap <C-I> :GFiles<CR>
+nnoremap <C-K> :BLines<CR>
+if executable('rg')
+    nnoremap <C-G> :Rg<CR>
+else
+    echom "Ripgrep (rg) is missing. Please install Ripgrep"
+endif
+]]
+
+-- auto-pairs-gentle
+vim.g.AutoPairs = {
+  ['(']=')',
+  ['[']=']',
+  ['{']='}',
+  ["'"]="'",
+  ['"']='"',
+  ['`']='`',
+  ['<']='>',
+}
+-- 1}}}
