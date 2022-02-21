a08d0c8de58d329ea956b204bc78ce671703f3f3
Arunanshu Biswas <mydellpc07@gmail.com>
2022-02-21 15:50:50 +0530

    fix(ohmybash): expand home variable

--- .config/yadm/bootstrap.d/ohmybash##o.Linux,e.sh
+++ .config/yadm/bootstrap.d/ohmybash##o.Linux,e.sh
@@ -1,6 +1,6 @@
 # manual installation of ohmybash
 git clone git://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
 
-[[ ! -e '~/.bashrc' ]] && cp ~/.oh-my-bash/templates/bashrc.osh-template ~/.bashrc
+[[ ! -e "$HOME/.bashrc" ]] && cp ~/.oh-my-bash/templates/bashrc.osh-template ~/.bashrc
 
 source ~/.bashrc
