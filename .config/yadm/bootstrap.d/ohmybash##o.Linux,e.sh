1221fac8c97a22d60b3922244a4bd255c98a7805
Arunanshu Biswas <mydellpc07@gmail.com>
2022-02-21 15:41:40 +0530

    fix(ohmybash): fix syntax

--- .config/yadm/bootstrap.d/ohmybash##o.Linux,e.sh
+++ .config/yadm/bootstrap.d/ohmybash##o.Linux,e.sh
@@ -1,6 +1,6 @@
 # manual installation of ohmybash
 git clone git://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
 
-[[ -ne '~/.bashrc' ]] && cp ~/.oh-my-bash/templates/bashrc.osh-template ~/.bashrc
+[[ ! -e '~/.bashrc' ]] && cp ~/.oh-my-bash/templates/bashrc.osh-template ~/.bashrc
 
 source ~/.bashrc
