9eecc29ea542f56f935a95b4f5ef637d293aee18
Arunanshu Biswas <mydellpc07@gmail.com>
2022-02-21 16:02:57 +0530

    fix(ohmybash): fix error

--- .config/yadm/bootstrap.d/ohmybash##o.Linux,e.sh
+++ .config/yadm/bootstrap.d/ohmybash##o.Linux,e.sh
@@ -1,6 +1,12 @@
 # manual installation of ohmybash
 OMB_PATH="$HOME/.oh-my-bash"
 BASHRC_PATH="$HOME/.bashrc"
-[[ ! -e $OMB_PATH ]] && git clone git://github.com/ohmybash/oh-my-bash.git $OMB_PATH
-[[ ! -e $BASHRC_PATH ]] && cp $OMB_PATH/templates/bashrc.osh-template $BASHRC_PATH
+if [[ ! -e $OMB_PATH ]]; then
+    echo "Cloning into $OMB_PATH"
+    git clone git://github.com/ohmybash/oh-my-bash.git $OMB_PATH
+fi
+if [[ ! -e $BASHRC_PATH ]]; then
+    echo "Copying template bashrc into '$OMB_PATH'"
+    cp $OMB_PATH/templates/bashrc.osh-template $BASHRC_PATH
+fi
 source $BASHRC_PATH
